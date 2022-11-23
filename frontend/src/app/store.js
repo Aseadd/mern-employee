cf5ba9e1352a86d9fe620e0de175ae4cb0329350
addis <betsega23@gmail.com>
2022-11-23 19:22:02 +0300

    Update store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
+import goalReducer from '../features/employees/employeeSlice'
 
 export const store = configureStore({
   reducer: {
