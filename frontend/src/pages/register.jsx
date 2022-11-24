df88a819ed50d89b54dd4805b675180f56ada686
addis <betsega23@gmail.com>
2022-11-24 15:08:26 +0300

    Add docker file

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -56,15 +56,6 @@ const Sectiontwo = styled.section`
   text-align: center;
 `;
 
-  const Input = styled.input`
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-`;
 
 function Register() {
   const [formData, setFormData] = useState({
